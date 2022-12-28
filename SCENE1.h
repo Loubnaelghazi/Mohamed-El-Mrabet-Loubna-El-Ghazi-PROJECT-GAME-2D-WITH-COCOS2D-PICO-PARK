c0b08fda0da44a3bd753fcf77034ce8056c4621b
Loubnaelghazi <114692135+Loubnaelghazi@users.noreply.github.com>
2022-12-28 22:00:45 +0100

    Update SCENE1.h

--- SCENE1.h
+++ SCENE1.h
@@ -1,21 +1,36 @@
 #pragma once
-#ifndef __SCENE1__SCENE_H__
-#define __SCENE1_SCENE_H__
+#ifndef SCENE1SCENE_H
+#define SCENE1_SCENE_H
 #include "cocos2d.h"
+#include "cocos2d.h"
+#include "physics/CCPhysicsWorld.h"
+#include "physics-nodes/CCPhysicsSprite.h"
+#include "physics/CCPhysicsContact.h"
+#include "Box2D/Box2D.h"
+#include "Box2D/Dynamics/b2WorldCallbacks.h"
+USING_NS_CC;
 using namespace cocos2d;
 
-USING_NS_CC;
 
 
-class SCENE1: public cocos2d::Layer
+class SCENE1 : public cocos2d::Layer
 {
 public:
     static cocos2d::Scene* createScene();
+
     virtual bool init();
-    cocos2d::Sprite* mysprite;
-    cocos2d::Menu* menu2;
+
     CREATE_FUNC(SCENE1);
     void go(Ref* pSender);
+
+    void reload(Ref *pSender); //fct de replacement de scene lors d echec
+    void play(Ref* pSender); //fct de passage vers scene lorsque le joueuer arrive a la porte
     
+  
+    bool onContactBegin(cocos2d::PhysicsContact& contact);
+       
+        
 };
+
+
 #endif
