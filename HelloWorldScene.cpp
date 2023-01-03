6776c82b337c9dd6aa28fb55957f346afc7ed6c7
Mohamed El Mrabet <118105032+med-el-mrabet@users.noreply.github.com>
2023-01-03 10:40:14 +0100

    Update HelloWorldScene.cpp

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -1,8 +1,10 @@
-#include "HelloWorldScene.h"
 #include "cocos2d.h"
 #include "SCENE1.h" //pour pouvoir switcher vers
-#include "AudioEngine.h"
-//biblio pour importer les audios
+#include "AudioEngine.h" //biblio pour importer les audioa
+#include "HelloWorldScene.h"
+#include "SCENE3.h"
+#include <iostream>
+
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
@@ -21,71 +23,50 @@ bool HelloWorld::init()
     if (!Layer::init()) {
         return false;
     }
+
     auto  origin = Director::getInstance()->getVisibleOrigin();
     auto visibleSize = Director::getInstance()->getVisibleSize();
-    // notre background/////////////////////////////////////////////////////////////
-   // auto background = Sprite::create("background.png");
-    //background->setAnchorPoint(Vec2(0, 0));
-    //background->setPosition(Vec2(0,0));
-    //this->addChild(background, 0);
-/////////////////////////////////////////////////////////////////////////
-// 
-//elements du menu ////////////////
-    //creation du menu 
-    //background de menu :
-    menuback = Sprite::create("background.png");
-    menuback->setPosition(Point((visibleSize.width /2)+origin.x,(visibleSize.height/2)+origin.y));
-  //  menuback->setAnchorPoint(Vec2(0, 0));
-    //menuback->setPosition(Vec2(0, 0));
-       this->addChild(menuback);
-        auto  *menu=Menu ::create();
-        menu->setPosition(Point(0, 0));
-        this->addChild(menu);
-///////////////////////////
-///ajout des elements:
-        auto menu_play = MenuItemFont::create("PLAY", CC_CALLBACK_1(HelloWorld::play,this)); //boutton play
-      //  auto menu_highscore = MenuItemFont::create("HIGHSCORES", CC_CALLBACK_1(HelloWorld::highscores, this));
-       // auto imgbutton = MenuItemFont::create("start3.jpg",CC_CALLBACK_1(HelloWorld::imgbutton,this));
 
-       menu_play->setPosition(Point(visibleSize.width/2,(visibleSize.height/3) ));
-       // imgbutton->setPosition(Point(visibleSize.width / 2, (visibleSize.height / 3) * 1));
-        //positionnement //2 au milieu
-       // menu_highscore->setPosition(Point(visibleSize.width / 2 , (visibleSize.height / 4) * 1)); //3 la fin 
-       // menu_highscore->setScale(0.5, 0.5);
-        //pour 1  c le 
-        //9smna 3la 4 cuz 3ndna 3 items, ila 3ndna 4 an9smu 3la 5 and so on...  
+    //////////////////menu background
 
-        //on peut utiliser tout simplement la fct alignitems qui fait le meme travail
-        menu_play->setColor(Color3B::RED);
-        auto *menu1 = Menu::create(menu_play,NULL);
-        menu1->setPosition(Point(0,0));
-        this->addChild(menu1);
+    auto menuback = Sprite::create("menu_background.png");
+    menuback->setPosition(Point((visibleSize.width / 2) + origin.x, (visibleSize.height / 2) + origin.y));
+    menuback->setScale(0.125);
 
-        //upload music
-        cocos2d::AudioEngine::preload("music.mp3"); //upload de notre music mp3 en v4 de cocos
-        cocos2d::AudioEngine::play2d("music.mp3",true);    //PLAY MUSIC //true whwnever t7lat hello world
-      
-                                                                    
+    ////////////////Pico Park LOGO
+
+    this->addChild(menuback);
+    auto* menu = Menu::create();
+    menu->setPosition(Point(0, 0));
+    this->addChild(menu);
+    auto start_button = Sprite::create("start_button.png");
+    auto start_button_selected = Sprite::create("start_button_selected.png");
+
+    //////////////Play button
+
+    MenuItemImage* menu_play = MenuItemImage::create();
+    menu_play->setNormalImage(start_button);
+    menu_play->setSelectedImage(start_button_selected);
+    menu_play->setCallback(CC_CALLBACK_1(HelloWorld::play, this));
+    menu->addChild(menu_play, 4);
+    menu_play->setPosition(Point(visibleSize.width / 2, (visibleSize.height / 2)));
+
+    /////////////upload music
+
+    cocos2d::AudioEngine::preload("music.mp3"); //upload de notre music mp3 en v4 de cocos
+    auto a=cocos2d::AudioEngine::play2d("music.mp3", true);    //PLAY MUSIC //true whwnever t7lat hello world
     return true;
 }
 
 
-        //implemetation des fcts crees :
-        void HelloWorld::play(cocos2d::Ref * PSender) {
-            CCLOG("PLAY"); //affichage de play 
-            //fct en cliquant sur play, on switch vers scene 1    
-            auto scene1 = SCENE1::createScene();
-            Director::getInstance()->pushScene(scene1); //la scene qu on veut y switch vers
-            cocos2d::AudioEngine::stopAll(); //stoper la music quand on clique sur play
-        }
-        //void HelloWorld::highscores(cocos2d::Ref* PSender) {
-          //  CCLOG("HIGHSCORES"); //affichage de play 
-        //}
 
-        void HelloWorld::imgbutton(cocos2d::Ref* Psender) {
-            CCLOG("IMAGE BUTTON");
-        }
+  ///Fonction pour aller au niveau 1 quand on click sur PLAY
 
-        
+void HelloWorld::play(cocos2d::Ref* PSender) {
+    auto scene1 = SCENE2::createScene();
+    Director::getInstance()->pushScene(TransitionSplitCols::create(1, scene1)); //la scene qu on veut y switch vers
+    cocos2d::AudioEngine::stopAll(); //stoper la music quand on clique sur play
+}
+
 
             
